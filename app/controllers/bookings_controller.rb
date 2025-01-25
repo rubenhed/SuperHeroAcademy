251b381ea076a6186a42f65a32deec35c23c2791
amy huang <flower850603@gmail.com>
2025-01-25 13:08:12 +0900

    landing page nav bar login button add

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,11 +16,29 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(
+    user_id: User.fist.id,
+    course_id: params[:course_id]
+  )
+
+    if @booking.save
+      flash[:alert] = "Booking successfully created!"
+      redirect_to course_path(@booking.course_id)
+    else
+      flash[:alert] = "Failed to create booking."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def new
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :course_id, :user_id)
   end
 end
